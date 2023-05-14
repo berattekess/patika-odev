82e58433abbf7a4e99ba38a64310b3e61f448de2
Berat Tekeş <berat.tekes@outlook.com>
2023-05-14 18:45:16 +0300

    ucgen

--- ucgen/src/Main.java
+++ ucgen/src/Main.java
@@ -2,20 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int yukseklik;
-        Scanner scanner =new Scanner(System.in);
+        int i, k, j, yukseklik;
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Üçgen için basamak değeri : ");
-        yukseklik =scanner.nextInt();
+        yukseklik = scanner.nextInt();
 
-        for (int i=1; i<yukseklik*2; i+= 2)
-        {
-            for (int k=0; k < (10 - i/2); k++)
+        for (i = 1; i < yukseklik * 2; i += 2) {
+            for (k = 0; k < (5 - i / 2); k++)
                 System.out.print(" ");
 
-            for (int j=0; j<i; j++)
+            for (j = 0; j < i; j++)
                 System.out.print("*");
 
             System.out.println("");
+        }
+        for (i = 0; i < yukseklik; i++) {
+            for (j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (k = 2 * yukseklik - 1; k >= (2 * i + 1); k--) {
+                System.out.print("*");
+
+            }
+            System.out.println();
         }
     }
 }
